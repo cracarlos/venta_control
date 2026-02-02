348cd60b21e869d37ed74b4314b8649cbb26c37e
Carlos Eduardo <carlos.etr@gmail.com>
2026-02-02 01:08:18 -0400

    Primer comentario

---
+++ Backend/users/urls.py
@@ -0,0 +1,12 @@
+# from django.urls import path, include
+# from rest_framework.routers import DefaultRouter
+# from .views import TaskViewSet
+
+# # Creamos el router local de la app
+# router = DefaultRouter()
+# router.register(r'users', , basename='task')
+
+# urlpatterns = [
+#     # Incluimos las rutas del router bajo la raíz de este archivo
+#     path('', include(router.urls)), 
+# ]
